464ab76d55b7653e16b37617e761022de735bb1a
Christa Stephens <christastephens@quickenloans.com>
2019-06-04 11:18:41 -0400

    adding correct jquery link

--- gen.js
+++ gen.js
@@ -7,3 +7,8 @@ var date1 = new Date();
 function quoteRefresh() {
     document.getElementById("demo").innerHTML = "Hello World";
   }
+
+
+  $.getJSON("https://api.weatherbit.io/v2.0/current?city=Detroit,MI&units=I&key=94e389333f9a48ffba944ba55675cb55",data => {
+    console.log(data);
+  });
