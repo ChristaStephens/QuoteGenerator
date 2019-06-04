0c3a738d3fe0ccb61dec1b2c3f014d33a23fd576
Christa Stephens <christastephens@quickenloans.com>
2019-06-04 10:20:59 -0400

    date and time

---
+++ gen.js
@@ -0,0 +1,9 @@
+// var currentTime = new Date();
+// moment(new Date).format("MMMM YYYY");
+
+
+var date1 = new Date();
+
+function quoteRefresh() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
