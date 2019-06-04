7fd13e70ea13ec202e41d92b2e06d188d57a2dff
Christa Stephens <christastephens@quickenloans.com>
2019-06-04 11:56:38 -0400

    adding icons

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ function quoteRefresh() {
   requesting JSON data created by WeatherBit. Arrow function
   is used in place of 'function(data)'
    */
-  $.getJSON("https://api.weatherbit.io/v2.0/current?city=Detroit,MI&units=I&key=94e389333f9a48ffba944ba55675cb55",data => {
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=detroit&units=imperial&appid=6ccdde3b5bf6715b81441f3f37bce188",data => {
     console.log(data);
+    var icon ="http://openweathermap.org/img/w/" + data.weather[0].icon +".png";
+
+    console.log(icon);
   });
