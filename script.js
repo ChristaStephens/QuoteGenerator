615838f76b06d99e604dbbf9b6fd610014756b52
Christa Stephens <christastephens@quickenloans.com>
2019-06-04 11:29:09 -0400

    fixed js script error, pages now match with links

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ function quoteRefresh() {
     document.getElementById("demo").innerHTML = "Hello World";
   }
 
-
+/*
+  requesting JSON data created by WeatherBit. Arrow function
+  is used in place of 'function(data)'
+   */
   $.getJSON("https://api.weatherbit.io/v2.0/current?city=Detroit,MI&units=I&key=94e389333f9a48ffba944ba55675cb55",data => {
     console.log(data);
   });
